96ad6eeb0d99b9dc911304aaad629539b5d6b8dc
Viktor Dmytrenko <viktor.dmytrenko19@gmail.com>
2022-11-07 21:20:02 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+const downBtn = document.querySelector('.down-button');
+const upBtn = document.querySelector('.up-button');
+const container = document.querySelector('.container');
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide')
+
+const slidesCount = mainSlide.querySelectorAll('div').length;
+
+let activeSlideIndex = 0;
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+downBtn.addEventListener('click', () => chandeSlide('down'));
+upBtn.addEventListener('click', () => chandeSlide('up'));
+
+function chandeSlide(dir) {
+  if (dir === 'up') {
+    activeSlideIndex++;
+    if (activeSlideIndex === slidesCount) {
+      activeSlideIndex = 0;
+    }
+  }
+  if (dir === 'down') {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesCount - 1;
+    }
+  }
+  const h = container.clientHeight; 
+  sidebar.style.transform = `translateY(${activeSlideIndex * h}px)`
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * h}px)`
+}
+
